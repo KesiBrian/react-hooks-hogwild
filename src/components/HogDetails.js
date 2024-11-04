d066301a28770d044788b5c32be7e26885e0c024
KesiBrian <briankesim@gmail.com>
2024-11-04 21:45:58 +0300

    origin

--- src/components/HogDetails.js
+++ src/components/HogDetails.js
@@ -9,8 +9,7 @@ function HogDetails({ hog }) {
         Highest medal achived: <strong>{medal}</strong>
       </p>
       <p>
-        Weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator
-        with Thru-the-Door Ice and Water: <strong>{weight}</strong>
+        Weight : <strong>{weight}</strong>
       </p>
     </div>
   )
